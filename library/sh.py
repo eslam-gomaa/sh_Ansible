9fd1ede4487346b05469f254c7f4082a28ea4a42
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2020-03-27 15:42:22 +0000

    add doc to the module

--- library/sh.py
+++ library/sh.py
@@ -1,8 +1,164 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2020, eslam.gomaa <linkedin.com/in/eslam-gomaa>
+
+DOCUMENTATION = '''
+---
+module: sh
+short_description: Run your shell commands in an idempotent way
+version_added: "1.0"
+description:
+    - Run your sell commands based on the output of another 'shell commands' or 'python code'
+        - If the condition not met, the command will not run and will be not marked as 'changed'
+        - the condition can be rc OR stdout OR (rc+stdout)
+
+options:
+    cmd:
+        description:
+            - A shell command to run on the remote hosts
+        required: true
+        type: str
+
+    condition:
+        description:
+            - A 'shell command' or 'python code' to run on the remote hosts as a condition
+        required: false
+        default: None
+        type: str
+
+    lang:
+        description:
+            - the language of the condition command/code
+        required: false
+        default: bash
+        type: str
+
+    if_rc:
+        description:
+            - RUN THE COMMAND -> if the 'rc' of the condition command/code matches the provided value
+        required: false
+        default: None
+        type: int
+
+    if_stdout:
+        description:
+            - RUN THE COMMAND -> if the 'if_stdout' of the condition command/code matches the provided value
+        required: false
+        default: None
+        type: str    
+
+author:
+    - eslam.gomaa (linkedin.com/in/eslam-gomaa)
+'''
+
+EXAMPLES = '''
+ ----------------------------------------------------
+
+# Example 1
+
+- name: run shell command based on the "stdout" of another shell command
+  sh:
+    cmd: 'hostname'
+    condition: 'hostname'
+    lang: bash
+    if_stdout: 'ansible1'
 
 
-#################
-##################################
+ ----------------------------------------------------
+
+# Example 2
+
+- name: run command based of the "rc" of another command --  multi line
+  sh:
+    cmd: |
+      export pass='password'
+      export users_file=/root/users.txt
+      echo 'user-test' > $users_file
+
+          if [ -f $1 ] ; then
+              for i in $( cat /root/users.txt ); do
+                 useradd "$i"
+                 echo "User $i created successfully"
+                 echo $pass | passwd "$i" --stdin
+              done
+          else
+              echo "Input is NOT a file"
+          fi
+    condition: 'cat /etc/passwd | grep user-test'
+    lang: bash
+    if_rc: 1
+
+ ----------------------------------------------------
+
+# Example 3
+
+- name: run shell command based on "stdout" of Python code
+  sh:
+    cmd: echo 'Execute some commands '
+    condition: |
+      import socket
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      result = sock.connect_ex(('127.0.0.1',22))
+      if result == 0:
+         print "open"
+      else:
+         print "not-open"
+      sock.close()
+    lang: python
+    if_stdout: 'open'
+
+ ----------------------------------------------------
+
+# Example 4
+
+- name: run shell command based on "stdout" + "rc" of Python code
+  sh:
+    cmd: echo 'Execute some commands '
+    condition: |
+      import socket
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      result = sock.connect_ex(('127.0.0.1',24))
+      if result == 0:
+         print "open"
+         sock.close()
+      else:
+         print "not-open"
+         sock.close()
+         exit(1)
+    lang: python
+    if_rc: 1
+    if_stdout: 'not-open'
+
+ ----------------------------------------------------    
+'''
+
+RETURN = '''
+cmd:
+    description: output of the command     
+    type: dict
+    returned: If the command did run
+
+cmd_run:
+    description: if == 0 then the command did run, if == 1 then the command did not run
+    type: int
+    returned: always
+
+condition:
+    description: output of the condition command/code
+    type: dict
+    returned: If the condition command did run    
+                 
+'''
+
+
+##############################################
+#######################
+############
 def runcommand(cmd):
+    """
+    function to execute shell commands and returns a dic of
+    """
     import subprocess
 
     info = {}
@@ -19,33 +175,35 @@ def runcommand(cmd):
     # strips all kinds of trailing whitespace by default, not just one newline
     return info
 
+
 def run_py_code(code, python_version='python'):
     import random
     import os
     import stat
 
-    file = '/tmp/{}.py'.format(random.randint(0,1000))
-    py_file = open(file,"w+")
+    file = '/tmp/{}.py'.format(random.randint(0, 1000))
+    py_file = open(file, "w+")
     py_file.write('#!/usr/bin/{}\n'.format(python_version))
     py_file.write('\n')
     py_file.write(code)
     # make file executable
-    os.chmod(file, stat.S_IRUSR | stat.S_IWRITE | stat.S_IEXEC | stat.S_IRGRP | stat.S_IROTH | stat.S_IXGRP | stat.S_IXOTH)
+    os.chmod(file,
+             stat.S_IRUSR | stat.S_IWRITE | stat.S_IEXEC | stat.S_IRGRP | stat.S_IROTH | stat.S_IXGRP | stat.S_IXOTH)
     # we need to close the file before executing the command otherwise 'runcommand' function will not execute
     py_file.close()
-    cmd = runcommand('{} {}'.format(python_version, file)) # Run the script and get the output
-    os.remove(file) # Remove the script file
+    cmd = runcommand('{} {}'.format(python_version, file))  # Run the script and get the output
+    os.remove(file)  # Remove the script file
     return cmd
 
-def shell2(cmd, lang,condition=None,  if_rc=None, if_stdout=None, env=False):
 
+def shell2(cmd, lang, condition=None, if_rc=None, if_stdout=None, env=False):
     info = {}
-    info['cmd']     = None
+    info['cmd'] = None
     info['cmd_run'] = None
     info['condition'] = {}
-    info['condition']['lang']   = lang
-    info['condition']['cmd']    = condition
-    info['condition']['if_rc']     = if_rc
+    info['condition']['lang'] = lang
+    info['condition']['cmd'] = condition
+    info['condition']['if_rc'] = if_rc
     info['condition']['if_stdout'] = if_stdout
 
     if condition is None:
@@ -131,12 +289,11 @@ def shell2(cmd, lang,condition=None,  if_rc=None, if_stdout=None, env=False):
                 info['condition']['if_stdout'] = if_stdout
 
     return info
-
-##################################
-#################
+############
+#######################
+##############################################
 
 from ansible.module_utils.basic import *
-
 
 def main():
     fields = {
@@ -156,18 +313,16 @@ def main():
                  if_stdout=module.params['if_stdout']
                  )
 
-    #cmd_args = module.params['cmd'].split()
+    # cmd_split = module.params['cmd'].split()
 
     if not run['cmd'] is None:
         if run['cmd']['stderr']:
             module.fail_json(msg=run['cmd'])
 
-
     if run['cmd_run'] == 1:
         changed_ = False
     else:
         changed_ = True
-
 
     module.exit_json(changed=changed_, meta=run)
 
